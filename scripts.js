cda636ce034b6a5c98d7ebf31e36ec9cce8ea1a0
Patrick Cameron <patcameron@gmail.com>
2018-10-15 16:23:54 -0400

    Use v-if attr to avoid flash of variable text.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-    $.getJSON('candidates.json', function(results) {
-
-        var dropdownMenu = new Vue({
-            el: '.js--vue-app',
-            data: {
-                wardData: results.data
-            }
-        });
-        
-    });
+    var vueApp = new Vue({
+        el: '.js--vue-app',
+        data: {
+            wardData: null
+        },
+        mounted: function() {
+            $.getJSON('candidates.json', function(results) {
+                vueApp._data.wardData = results.data;
+            })
+        }
+    })
 });
