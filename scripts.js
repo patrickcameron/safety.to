f3119d2ed3ddf694b78dbe18d4c9d3ddb5ea04a8
Patrick Cameron <patcameron@gmail.com>
2018-10-16 22:31:22 -0400

    Reorganize js logic.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-    var vueApp = new Vue({
-        el: '.js--vue-app',
-        data: {
-            wardData: null
-        },
-        mounted: function() {
-            $.getJSON('candidates.json', function(results) {
-                vueApp._data.wardData = results.data;
-            })
-        }
-    })
-});
+var vueApp = new Vue({
+    el: '.js--vue-app',
+    data: {
+        wardData: null
+    },
+    mounted: function() {
+        getCandidateData();
+    }
+})
+
+function getCandidateData() {
+    $(document).ready(function() {
+        $.getJSON('candidates.json', function(results) {
+            vueApp.wardData = results.data;
+        })
+    });
+}
